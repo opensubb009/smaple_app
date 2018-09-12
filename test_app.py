766fff3330b7708e8aa79c652fe21279131afb11
opensubb009 <sinhasubbu456@gmail.com>
2018-09-12 14:11:46 +0530

    test_app
    new test case

---
+++ test_app.py
@@ -0,0 +1,21 @@
+import unittest
+from app import app
+
+
+class TestApp(unittest.TestCase):
+
+	def setUp(self):
+		print('Test working')
+		self.app = app.test_client()
+		self.app.testing = True 
+		
+	def test_endpoint(self):
+		result = self.app.get('/')
+		print('result', result.get_data())
+		self.assertEqual(result.status_code, 200)
+		self.assertEqual(result.get_data(), b'200 OK')
+
+
+
+if __name__ == "__main__":
+	unittest.main()
