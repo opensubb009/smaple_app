8ce92a49989220decd015b43b3061e6d6b751d71
opensubb009 <sinhasubbu456@gmail.com>
2018-09-13 16:37:26 +0530

    new changes
    data

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask
-
+from flask_restplus import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
+@api.route('/hello')
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
 
-@app.route('/')
-def index():
-	return '200 OK'
-
-
-if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
